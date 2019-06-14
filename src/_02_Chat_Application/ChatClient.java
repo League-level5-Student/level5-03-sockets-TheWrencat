66034dfbe62cf300bcae06426068b89d90bdddb8
League Student <league@iMac-2.local>
2019-06-13 19:04:39 -0700

    finished up

--- src/_02_Chat_Application/ChatClient.java
+++ src/_02_Chat_Application/ChatClient.java
@@ -9,34 +9,23 @@ import java.io.*;
 public class ChatClient {
 
 	public static void main(String[] args) {
-		// 1. Create a String for the ip address of the server.
-		// If you don't know how to find a computer's ip address, ask about ifconfig on
-		// linux/mac and ipconfig on windows.
 		String ip = "192.168.1.174";
-		// 2. Create an integer for the server's port number
 		int port = 8001;
-		// 3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
+		DataInputStream dataInput;
 		try {
-			// 4. Create an object of the Socket class. When initializing the object, pass
-			// in the ip address and the port number
-
 			Socket connection = new Socket(ip, port);
-			JOptionPane.showMessageDialog(null, "Connected to Server.");
-			// 5. Create a DataOutputStream object. When initializing it, use the Socket
-			// object you created in step 4 to call the getOutputStream() method.
+			JOptionPane.showMessageDialog(null, "Connected to Server.", "Client", JOptionPane.PLAIN_MESSAGE);
 			DataOutputStream dataOutput = new DataOutputStream(connection.getOutputStream());
-			// 6. Use the DataOutputStream object to send a message to the server using the
-			// writeUTF(String message) method
-			String output = JOptionPane.showInputDialog("What would you like to say?");
-			dataOutput.writeUTF(output);
-			// 7. Create a DataInputStream object. When initializing it, use the Server
-			// object you created in step 4 to call the getInputStream() method.
-			DataInputStream dataInput = new DataInputStream(connection.getInputStream());
-			// 8. Use the DataInputStream object to print a message from the server using
-			// the readUTF() method.
-			JOptionPane.showMessageDialog(null, dataInput.readUTF());
-			// 9. Close the client's server object
+			
+			do {
+				 	
+				String output = JOptionPane.showInputDialog(null, "What would you like to say?", "Client", JOptionPane.PLAIN_MESSAGE);
+				dataOutput.writeUTF(output);
+				dataInput = new DataInputStream(connection.getInputStream());
+				JOptionPane.showMessageDialog(null, dataInput.readUTF());
+			} while (!dataInput.readUTF().equalsIgnoreCase("bye"));
 			connection.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/_02_Chat_Application/ChatServer.java
+++ src/_02_Chat_Application/ChatServer.java
@@ -7,80 +7,51 @@ import javax.swing.JOptionPane;
 import java.io.*;
 
 public class ChatServer extends Thread {
-	// 1. Create an object of the ServerSocket class
 	ServerSocket server;
 	int port = 8001;
 
 	public ChatServer() throws IOException {
-		// 2. Initialize the ServerSocket object. In the parameters,
-		// you must define the port at which the server will listen for connections.
 		server = new ServerSocket(port, 100);
-		// *OPTIONAL* you can set a time limit for the server to wait by using the
-		// ServerSocket's setSoTimeout(int timeInMilliSeconds) method
 		server.setSoTimeout(15000000);
 	}
 
 	public void run() {
-		// 3. Create a boolean variable and initialize it to true.
 		boolean var = true;
-		// 4. Make a while loop that continues looping as long as the boolean created in
-		// the previous step is true.
+		DataInputStream dataInput;
 		while (var) {
 
-			// 5. Make a try-catch block that checks for two types Exceptions:
-			// SocketTimeoutException and IOException.
-			// Put steps 8 - 15 in the try block.
 			try {
-
-				// 8. Let the user know that the server is waiting for a client to connect.
-
-				JOptionPane.showMessageDialog(null, "Waiting for a client to connect..");
-				// 9. Create an object of the Socket class and initialize it to
-				// serverSocket.accept();
-				// Change serverSocket to match the ServerSocket member variable you created in
-				// step 1.
-				// The program will wait here until either a client connects or the timeout
-				// expires.
+				JOptionPane.showMessageDialog(null, "Waiting for a client to connect..", "Server",
+						JOptionPane.PLAIN_MESSAGE);
 				Socket sock = server.accept();
-				
-				// 10. Let the user know that the client has connected.
-				JOptionPane.showMessageDialog(null, "Client Connected!");
-				// 11. Create a DataInputStream object. When initializing it, use the Socket
-				// object you created in step 9 to call the getInputStream() method.
-				DataInputStream dataInput =  new DataInputStream( sock.getInputStream());
-				// 12. Print the message from the DataInputStream object using the readUTF()
-				// method
-				JOptionPane.showMessageDialog(null, dataInput.readUTF());
-				// 13. Create a DataOutputStream object. When initializing it, use the Server
-				// object you created in step 9 to call the getOutputStream() method.
-				DataOutputStream dataOutput = new DataOutputStream(sock.getOutputStream());
-				// 14. Use the DataOutputStream object to send a message to the client using the
-				// writeUTF(String message) method.
-				String output = JOptionPane.showInputDialog("What would you like to send?");
-				dataOutput.writeUTF(output);
-				// 15. Close the client server
+
+				JOptionPane.showMessageDialog(null, "Client Connected!", "Server", JOptionPane.PLAIN_MESSAGE);
+				do {
+					dataInput = new DataInputStream(sock.getInputStream());
+					JOptionPane.showMessageDialog(null, dataInput.readUTF());
+					DataOutputStream dataOutput = new DataOutputStream(sock.getOutputStream());
+					String output = JOptionPane.showInputDialog(null, "What would you like to say?", "Server",
+							JOptionPane.PLAIN_MESSAGE);
+					dataOutput.writeUTF(output);
+				} while (!dataInput.readUTF().equalsIgnoreCase("bye"));
 				server.close();
 			} catch (SocketTimeoutException e) {
 				var = false;
 			} catch (IOException e) {
 				var = false;
 			}
-			
+
 		}
 
 	}
 
 	public static void main(String[] args) {
-		// 16. In a new thread, create an object of the ServerGreeter class and start
-		// the thread. Don't forget the try-catch.
 		try {
 			ChatServer hello = new ChatServer();
 			hello.run();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 	}
 }
